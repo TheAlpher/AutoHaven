3d44ee44eb1a7f215092be425978eb5b4bc2dc6c
Piyush <gupta99piyush@gmail.com>
2019-10-13 02:29:33 +0530

    v5

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,4 +1,4 @@
-const UserModel = require("../model/UserModel");
+const UserModel = require("../model/usermodel");
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
